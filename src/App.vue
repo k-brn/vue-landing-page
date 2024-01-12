69c3d7dbdf545d4dec4bdd95c126f3668c1e1079
k-brn <108495284+k-brn@users.noreply.github.com>
2024-01-12 00:34:26 -0800

    Mobile bug fixes in nav bar and about sections

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <div class="text-lg font-bold">Paperclip Studios</div>
 
         <!-- Navigation Links -->
-        <div class="space-x-4">
+        <div class="flex space-x-4">
           <router-link to="/" class="no-underline hover:underline"
             >Home</router-link
           >
@@ -18,20 +18,16 @@
             >Portfolio</router-link
           >
           <router-link to="/about" class="no-underline hover:underline"
-            >About Us</router-link
+            >About</router-link
           >
           <router-link to="contact" class="no-underline hover:underline"
             >Contact</router-link
           >
         </div>
-
-        <!-- Responsive Menu -->
-        <div class="lg:hidden">
-          <!-- Add responsive menu icon/button here -->
-        </div>
       </div>
     </nav>
   </div>
+
   <!-- Hero -->
   <section
     class="hero-section relative overflow-hidden bg-soft-blue h-screen flex items-center"
@@ -62,22 +58,20 @@
       </router-link>
     </div>
   </section>
-  <!-- About Us -->
+  <!-- About -->
   <section
     class="about-section bg-black py-16 flex flex-col-reverse items-center lg:flex-row"
   >
     <div
-      class="container mx-auto flex flex-col lg:flex-row justify-between items-center"
+      class="container mx-auto px-4 flex flex-col lg:flex-row justify-between items-center"
     >
-      <!-- About Us Text (To the Left) -->
       <div class="w-full lg:w-1/2 lg:pr-8 mb-8 lg:mb-0">
-        <!-- Section Title -->
-        <h2 class="text-creamy-white text-3xl lg:text-4xl font-bold mb-8">
-          About Us
+        <h2 class="text-3xl lg:text-4xl font-bold text-creamy-white mb-8">
+          About
         </h2>
 
         <!-- Description -->
-        <p class="text-creamy-white text-lg mb-4">
+        <p class="text-creamy-white text-base lg:text-lg mb-4">
           Paperclip Studios is a creative hub dedicated to digital design and
           crafting unique products. Our mission is to unleash creativity and
           bring innovative ideas to life. We are passionate about delivering
@@ -85,19 +79,19 @@
           clients and users.
         </p>
 
-        <p class="text-creamy-white text-lg mb-4">
+        <p class="text-creamy-white text-base lg:text-lg mb-4">
           Our team comprises skilled individuals with expertise in various areas
           of digital design.
         </p>
 
-        <p class="text-creamy-white text-lg mb-4">
+        <p class="text-creamy-white text-base lg:text-lg mb-4">
           Over the years, we have successfully delivered digital solutions to a
           diverse range of clients, earning recognition for our creativity and
           quality.
         </p>
 
         <!-- Call to Action -->
-        <p class="text-creamy-white text-lg">
+        <p class="text-creamy-white text-base lg:text-lg">
           Ready to collaborate with us?
           <a href="#" class="no-underline hover:underline"
             >Contact Paperclip Studios</a
@@ -116,58 +110,60 @@
       </div>
     </div>
   </section>
+
   <!-- Portfolio -->
-  <section class="portfolio-section bg-gray-100 py-16">
+  <section class="portfolio-section py-16">
     <div class="container mx-auto">
       <!-- Portfolio Title -->
       <h2 class="text-3xl font-bold mb-8">Featured Work</h2>
 
       <!-- Portfolio Items Container -->
-      <div class="flex mb-8">
+      <div class="flex flex-col lg:flex-row mb-8">
         <!-- Portfolio Item 1 -->
-        <div class="w-1/2 pr-8">
+        <div class="lg:w-1/2 pr-8 mb-8 lg:mb-0">
           <!-- Image -->
           <img
             src="./assets/images/portfolio-item1.jpg"
             alt="Portfolio Item 1"
-            class="w-full h-full object-cover mb-4"
+            class="w-full h-full object-cover mb-4 lg:mb-0"
           />
 
           <!-- Details (Text) -->
-          <div>
-            <p class="text-soft-blue text-lg mb-2">
+          <div class="lg:ml-6">
+            <p class="text-lg mb-2">
               Website URL:
               <a href="https://www.example.com" target="_blank"
                 >www.NovaSyncTechnologies.com</a
               >
             </p>
-            <p class="text-black text-sm">Year: 2022</p>
+            <p class="text-sm mb-4">Year: 2022</p>
           </div>
         </div>
 
         <!-- Portfolio Item 2 -->
-        <div class="w-1/2 pl-8">
+        <div class="lg:w-1/2 pl-8">
           <!-- Image -->
           <img
             src="./assets/images/portfolio-item2.jpg"
             alt="Portfolio Item 2"
-            class="w-full h-full object-cover mb-4"
+            class="w-full h-full object-cover mb-4 lg:mb-0"
           />
 
           <!-- Details (Text) -->
-          <div>
-            <p class="text-soft-blue text-lg mb-2">
+          <div class="lg:mr-6">
+            <p class="text-lg mb-2">
               Website URL:
               <a href="https://www.anotherexample.com" target="_blank"
                 >www.QuantumByteSolutions.com</a
               >
             </p>
-            <p class="text-black text-sm">Year: 2021</p>
+            <p class="text-sm mb-4">Year: 2021</p>
           </div>
         </div>
       </div>
     </div>
   </section>
+
   <!-- Footer -->
   <footer class="bg-black text-creamy-white py-8">
     <div class="container mx-auto flex items-center justify-between">
